362d5288110fcc6b937770647a4b78181e504998
Alex Simes <alex.simes29@gmail.com>
2015-03-11 13:24:04 +0000

    added gizems submit screen, fixed tabulation

--- app/pages/submit.php
+++ app/pages/submit.php
@@ -12,16 +12,40 @@
     <!-- Header -->
     <?php print_header($app_directory); ?>
     <!-- Main -->
-      <div id="main-wrapper">
-        <div class="container">
+    <div id="main-wrapper">
+      <div class="container">
+        <!-- Content -->
+        <div id="content">
+        <!-- this is where the content will go -->
+
           <div class="row">
-              <!-- Content -->
-                <div id="content">
-		<!-- this is where the content will go -->
-                </div>
-            </div>
-          </div>
-        </div>
-      </div>
+            <div class="3u">
+              <h2>Submit now!</h2>
+              <div id="envelope">
+                <form action="" method="post">
+                  <label> First Name </label>
+                  <input type="text" name="firstname" width="100px;">
+
+                  <label> Last Name </label>
+                  <input type="text" name="lastname" width="100px;">
+
+                  <label> Email </label>
+                  <input type="text" name="email">
+
+                  <label> Username </label>
+                  <input type="text" name="username">
+
+                  <label> Password </label>
+                  <input type="password" name="password">
+                    
+                  <input type="submit" value="Submit" id="submit">
+                </form>
+              </div><!--envelope-->
+            </div><!--class 3u-->
+          </div><!--row-->
+
+        </div><!--content-->
+      </div><!--container-->
+    </div><!--main_wrapper-->
   </body>
 </html>
