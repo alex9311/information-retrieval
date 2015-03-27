f4649291cee7d6248778ce2eb6a6c562c4e51ee8
M.H.Doorn <millenniumproof@gmail.com>
2015-03-27 11:35:54 +0100

    Added security check to receiving CrowdFlower results

--- app/pages/GetJSONresultsCrowdFlower.php
+++ app/pages/GetJSONresultsCrowdFlower.php
@@ -4,20 +4,20 @@
 	$conn = connect_db(); 
 	// Convert JSON object to php associative array 
 	$data = json_decode($_POST['payload'], true);
-	
-	if($_POST['signal'] == "unit_complete"){
+	// Check signal type and signature for security
+	$api_key = "vREt_GNtauNnKc3y1JXX";
+	if($_POST['signal'] == "unit_complete" && $_POST['signature']===sha1($_POST['payload'].$api_key)){
 		// Get the results 
 		$id_idea = $data['data']['id']; 
 		$acceptedstring = $data['results']['should_this_idea_be_rejected']['agg'];
 		$reason = $data['results']['why_should_this_idea_be_rejected']['agg'];
 		// Insert into the table 
 			$sql = "";
+			$accepted = -1;
 			if($acceptedstring=="Yes"){
 				$accepted = 0;
 			}else if($acceptedstring=="No"){
 				$accepted = 1;
-			}else if($acceptedstring=="Not Sure"){
-				$accepted = -1;
 			}
 			$sql =  'INSERT INTO Screening_results_crowdflower(id_idea, accepted, reason) VALUES("'.$id_idea.'", "'.$accepted.'", "'.$reason.'")';
 			if($sql!==""){
