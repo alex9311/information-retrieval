e29fd73332e4caaf35ee700289d75ba298dff33d
PeterB <petervanbuul@gmail.com>
2015-03-28 21:16:00 +0000

    added top 5 idea page

--- app/pages/idea.php
+++ app/pages/idea.php
@@ -5,20 +5,25 @@
   Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-  <?php include "../common/common_functions.php";?>
   <?php include "../common/common.php";?>
-  <?php include "top_ideas_functions.php";?>
+  <?php include "vote_functions.php";?>
   <?php print_imports($app_directory); ?>
   <body class="no-sidebar">
     <!-- Header -->
     <?php print_header($app_directory); ?>
     <!-- Main -->
-    <div id="main-wrapper" style=“padding-top:1em;">
+    <div id="main-wrapper">
         <!-- Content -->
         <div id="content">
         <!-- this is where the content will go -->
-	<?php get_top_ideas($app_directory);?>
+          <div id="vote">
+		<?php $idea = get_idea($_GET["id"]); ?>
+            <img class="ui large image vote_image" src="<?php echo substr($idea["image"],13);?>">
+            <div class="ui large header" id="text"><?php echo $idea["title"]; ?></div>
+            <div class="vote_descrip_text"><p><?php echo $idea["text_description"]; ?></p></div>
+          </div>
         </div><!--content-->
     </div><!--main_wrapper-->
   </body>
 </html>
+
--- app/pages/top_ideas_functions.php
+++ app/pages/top_ideas_functions.php
@@ -4,17 +4,21 @@ include "../common/database_functions.php";
 
 
 
-function get_top_ideas(){
+function get_top_ideas($app_directory){
 	$conn = connect_db();
 	$sql = "Select id, title,text_description,image,upvotes from Idea ORDER BY upvotes LIMIT 5";
         $result = execute_mysql($sql,$conn);
-	echo '<table border="1">';
-        echo' <tr><td>Title</td><td>Desciption</td></td></thead>';
+	echo '<table border="1" id="top-5-table">';
+        echo' <tr><td>Rank</td><td>Title</td></tr></thead>';
+	$rank = 1;
         while($row = $result->fetch_assoc()) {
+		$title = $row["title"];
+		$id = $row["id"];
                 echo '<tr>';
-               	echo '<td>'.$row["title"]."</td>";
-               	echo '<td>'.$row["text_description"]."</td>";
-                echo '</tr>';
+		echo '<td>'.$rank."</td>";
+               	echo '<td><a href="'.$app_directory.'/pages/idea.php/?id='.$id.'">'.$title."</a></td>";
+		echo '</tr>';
+		$rank = $rank + 1;
         }
 	echo '</table>';
 }
