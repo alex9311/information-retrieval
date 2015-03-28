8b73d7ff57d7b927ac16ab80ae703db3e2e221cc
Alex Simes <alex.simes29@gmail.com>
2015-03-28 15:22:09 +0000

    removed un-needed functions

--- app/pages/top_ideas_functions.php
+++ app/pages/top_ideas_functions.php
@@ -4,24 +4,6 @@ include "../common/database_functions.php";
 
 
 
-function upvote_idea($idea_id){
-	$conn = connect_db();
-        $sql = "Select upvotes from Idea where id = ".$idea_id." LIMIT 1";
-	$result = execute_mysql($sql,$conn);
-
-	$old_upvote = $result->fetch_assoc();
-
-	$new_upvote = (int)($old_upvote["upvotes"]) +1;
-
-	$sql = "UPDATE Idea SET upvotes='".$new_upvote."' WHERE id=".$idea_id;
-	
-	// Prepare statement
-	$stmt = $conn->prepare($sql);
-	
-	$stmt->execute();
-       	$conn->close();
-}
-
 function get_top_ideas(){
 	$conn = connect_db();
 	$sql = "Select id, title,text_description,image,upvotes from Idea ORDER BY upvotes LIMIT 5";
@@ -37,17 +19,5 @@ function get_top_ideas(){
 	echo '</table>';
 }
 
-function get_idea($idea_id){
-        $conn = connect_db();
-        // Check connection
-        if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = "Select id, title, text_description, image from Idea where id = ".$idea_id." LIMIT 1";
-        $result = execute_mysql($sql,$conn);
-	
-	return $result->fetch_assoc();
-}
 
 ?>
