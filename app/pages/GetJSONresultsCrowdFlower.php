5d374bdf7b17b3d613c5d10b8ad0026fe165f60e
M.H.Doorn <millenniumproof@gmail.com>
2015-03-29 10:19:23 +0200

    TestSet csv added to documents & some code clean-up

--- app/pages/GetJSONresultsCrowdFlower.php
+++ app/pages/GetJSONresultsCrowdFlower.php
@@ -11,24 +11,18 @@
 		$id_idea = $data['data']['id']; 
 		$acceptedstring = $data['results']['should_this_idea_be_rejected']['agg'];
 		$reason = $data['results']['why_should_this_idea_be_rejected']['agg'];
-		// Insert into the table 
-			$sql = "";
-			$accepted = -1;
-			if($acceptedstring=="Yes"){
-				$accepted = 0;
-			}else if($acceptedstring=="No"){
-				$accepted = 1;
-			}
-			$sql =  'INSERT INTO Screening_results_crowdflower(id_idea, accepted, reason) VALUES("'.$id_idea.'", "'.$accepted.'", "'.$reason.'")';
-			if($sql!==""){
-				//and insert!
-				if ($conn->query($sql) === TRUE) {
-					echo "New record created successfully";
-				} else {
-					echo "Error: " . $sql . "<br>" . $conn->error;
-				}
-			}
+		$accepted = -1;
+		if($acceptedstring=="Yes"){
+			$accepted = 0;
+		}else if($acceptedstring=="No"){
+			$accepted = 1;
+		}
+		// Send results to database
+		$sql =  'INSERT INTO Screening_results_crowdflower(id_idea, accepted, reason) VALUES("'.$id_idea.'", "'.$accepted.'", "'.$reason.'")';
+		$conn->query($sql);
 		$conn->close();
+		// TODO: If $accepted is 0 send notification to the user. If $accepted is -1 send idea to admin for judgement.
+		// TODO: Create error log to catch and document errors
 	}
 ?>
 
