e5ac7d7907549d3b12fc64a8c92d7bdf4a4c7157
Alex Simes <alex.simes29@gmail.com>
2015-03-29 17:55:39 +0000

    added uploading ideas without image

--- app/pages/submit_functions.php
+++ app/pages/submit_functions.php
@@ -6,54 +6,58 @@ include "../common/connect.php";
 
 //only want to execute all these checks if we have a submission request
 if($_POST['formSubmit'] == "Submit"){	
-	//first figure out where the image will be stored and get file type
-	$target_dir = "/var/www/html/uploads/";
-	$digits = 4;
-	$image_prefix = rand(pow(10, $digits-1), pow(10, $digits)-1);
-	$target_file = $target_dir . $image_prefix. basename($_FILES["fileToUpload"]["name"]);
-	$uploadOk = 1;
-	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	if(strcmp("",$_FILES["fileToUpload"]["name"])!=0){
+		//first figure out where the image will be stored and get file type
+		$target_dir = "/var/www/html/uploads/";
+		$digits = 4;
+		$image_prefix = rand(pow(10, $digits-1), pow(10, $digits)-1);
+		$target_file = $target_dir . $image_prefix. basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-	//check to make sure the directory where we want to store the file exist and are writable
-	if (!file_exists($target_dir)){
-		return_to_submit_page('Upload directory does not exist, contant sys admin');
-	}
-	if(!is_writable($target_dir)) {
-		return_to_submit_page('Upload directory is not writable, contact sys admin.');
-	}
+		//check to make sure the directory where we want to store the file exist and are writable
+		if (!file_exists($target_dir)){
+			return_to_submit_page('Upload directory does not exist, contant sys admin');
+		}
+		if(!is_writable($target_dir)) {
+			return_to_submit_page('Upload directory is not writable, contact sys admin.');
+		}
 
-	// Check if image file is a actual image or fake image
-	if(isset($_POST["submit"])) {
-	    	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	    	if($check !== false) {
-	        	$uploadOk = 1;
-	    	} else {
-	        	$uploadOk = 0;
-	        	return_to_submit_page("File is not an image.");
-	    	}
-	}
-	// Check if file already exists
-	if (file_exists($target_file)) {
-	    	$uploadOk = 0;
-	    	return_to_submit_page("Sorry, file already exists.");
-	}
-	// Check file size
-	if ($_FILES["fileToUpload"]["size"] > 10000000) {
-	    	$uploadOk = 0;
-	    	return_to_submit_page("Sorry, your file is too large.");
-	}
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-	    	$uploadOk = 0;
-	    	return_to_submit_page("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
-	}
-
-	//now that we know the image is valid, actually try to move the image into the target directory
-	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		//if successful in storing image, add the idea to the database and return to the submit page!
-		insert_idea($_POST['title'],$_POST['text_description'],$target_file);
-	} else {
-		return_to_submit_page("Sorry, there was an error uploading your file.");
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+		    	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		    	if($check !== false) {
+		        	$uploadOk = 1;
+		    	} else {
+		        	$uploadOk = 0;
+		        	return_to_submit_page("File is not an image.");
+		    	}
+		}
+		// Check if file already exists
+		if (file_exists($target_file)) {
+		    	$uploadOk = 0;
+		    	return_to_submit_page("Sorry, file already exists.");
+		}
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 10000000) {
+		    	$uploadOk = 0;
+		    	return_to_submit_page("Sorry, your file is too large.");
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+		    	$uploadOk = 0;
+		    	return_to_submit_page("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+		}
+	
+		//now that we know the image is valid, actually try to move the image into the target directory
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			//if successful in storing image, add the idea to the database and return to the submit page!
+			insert_idea($_POST['title'],$_POST['text_description'],$target_file);
+		} else {
+			return_to_submit_page("Sorry, there was an error uploading your file.");
+		}
+	}else{
+                insert_idea($_POST['title'],$_POST['text_description'],"");
 	}
 }
 
