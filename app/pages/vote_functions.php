0a72b016a79016430b07fc24636fcc2e42ea348b
M.H.Doorn <millenniumproof@gmail.com>
2015-04-01 11:35:25 +0200

    Added Crowdflower check to voting page
    Check for correctness, please.

--- app/pages/vote_functions.php
+++ app/pages/vote_functions.php
@@ -28,10 +28,23 @@ function get_idea($idea_id){
         if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
         }
-
-        $sql = "Select id, title, text_description, image from Idea where id = ".$idea_id." LIMIT 1";
-        $result = execute_mysql($sql,$conn);
-	
+		$foundid = false;
+		while($foundid == false){
+			$sql = "Select id, title, text_description, image from Idea where id = ".$idea_id." LIMIT 1";
+			$result = execute_mysql($sql,$conn);
+			
+			// Check if idea has been accepted by the CrowdFlower sanitation check
+			$sql = "SELECT accepted FROM Screening_results_crowdflower WHERE id_idea = ".$idea_id;
+			$result2 = execute_mysql($sql,$conn);
+			if(mysqli_num_rows($results2) != 0){
+				$checkedbycf = $result2->fetch_assoc();
+				if($checkedbycf["accepted"]==1){
+					$foundid = true;
+				}
+			}
+			// If idea has not been accepted check the next idea;
+			$idea_id = $idea_id + 1;
+		} 
 	return $result->fetch_assoc();
 }
 
